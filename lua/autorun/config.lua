5e96d605af43d7c979c9e45ea976fd1989c21450
Adam Wilson <adamwilson790@aol.co.uk>
2014-11-29 18:36:03 +0000

    Spacing fix

--- lua/autorun/config.lua
+++ lua/autorun/config.lua
@@ -10,10 +10,10 @@
 	For NPIGamers: http://npigamers.com/
 --]]
 
-local warConfig = {}				 -- Don't touch this
-warConfig.team1 = { }				 -- First team name
-warConfig.team1Leader = {}			 -- Leader for the first team
-warConfig.team2 = { }			     -- Second team name
-warConfig.team2Leader = {}			 -- Leader for the second team
-warConfig.length = 900				 -- Length the war should last for
+local warConfig = {}                 -- Don't touch this
+warConfig.team1 = { }                -- First team name
+warConfig.team1Leader = {}           -- Leader for the first team
+warConfig.team2 = { }                -- Second team name
+warConfig.team2Leader = {}           -- Leader for the second team
+warConfig.length = 900               -- Length the war should last for
 warConfig.endWhenLeaderKilled = true -- When the leader is killed, should the war be over?
